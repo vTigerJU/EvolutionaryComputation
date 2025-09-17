eccc0d5509a3da0ad22721b84b843935286ff1ed
Ferresmash <120678062+Ferresmash@users.noreply.github.com>
2025-09-17 10:31:02 +0200

    removed 1 line

--- src/main.py
+++ src/main.py
@@ -7,7 +7,6 @@ if __name__ == "__main__":
     board = Board(5)
     print("8x8 Chess Board:")
     board.print_board_only()
-    board.print_positions
     board.evaluate()
 
     # test_collision_detection()
